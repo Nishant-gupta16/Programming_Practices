602a670d42a727240b4c5fc058838a03b5d8b5ff
Nishant-gupta16 <168644246+Nishant-gupta16@users.noreply.github.com>
2024-05-02 11:33:20 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,12 @@
+class InnerClass {
+    void display() {
+        System.out.println("Hello from InnerClass");
+    }
+}
+public class Assignment1
+{
+	public static void main(String[] args) {
+		InnerClass obj = new InnerClass();
+        obj.display();
+	}
+}
